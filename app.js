12138a9e468a9576d48384018de59dcd6f5e1b07
Waclaw Beletsky <hardhill@live.ru>
2019-10-03 21:08:06 +0800

    работа со статикой

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const pathhtml = config.wwwroot
 const hostname = config.hostname
 const port = config.port
 const app = express()
+app.use('/',express.static(pathhtml))
+
 app.get('/',(req,res)=>{
     res.sendFile(pathhtml+'/index.html')
 })
+app.post('/srv',(req,res)=>{
+
+})
 app.listen(port, hostname, ()=>{
-    var timestart = Date.now()
-    console.log(`Server PALLADIUM started on ${port} and ${hostname} hostname at ${timestart}`)
+    var timestart = Date(Date.now()).toString()
+    console.log(`Server PALLADIUM started on port ${port} and ${hostname} hostname at ${timestart}`)
 })
